d779f7e3f77b44146aef5a9adf62373952753426
Lukas Momot <lukasz.momot@hotmail.com>
2020-06-29 16:38:23 +0200

    Add bootstrap and react-bootstrap to solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./App.module.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
@@ -15,7 +16,17 @@ function App() {
 }
 
 function Home() {
-  return <h2 className={styles.heading}>Home - Digital Broker</h2>;
+  // return <h2 className={styles.heading}>Home - Digital Broker</h2>;
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2 className={styles.heading}>Home - Digital Broker</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
