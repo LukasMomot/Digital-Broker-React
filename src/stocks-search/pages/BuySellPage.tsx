0b79090dc758cefb3fd4fdf0a3d6feaa222ae064
Lukas Momot <lukasz.momot@hotmail.com>
2020-07-10 11:30:48 +0200

    Add fetch stock for buy sell

--- src/stocks-search/pages/BuySellPage.tsx
+++ src/stocks-search/pages/BuySellPage.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
+import { fetchStockPrice } from "../../businessLogic/stocksService";
+import { useEffect, useState } from "react";
+import { StockPrice } from "../../businessLogic/stockPrice";
 
 interface IBuySellPageProps {}
 
 const BuySellPage: React.FunctionComponent<IBuySellPageProps> = (props) => {
   const { symbol } = useParams();
-  return <h2>Buy Sell Page - Activated for {symbol}</h2>;
+  const [stock, setStock] = useState<StockPrice>();
+
+  useEffect(() => {
+    fetchStockPrice(symbol).then((stock) => setStock(stock));
+  }, [symbol]);
+
+  return <h2>Buy Sell Page - Activated for {stock?.name}</h2>;
 };
 
 export default BuySellPage;
